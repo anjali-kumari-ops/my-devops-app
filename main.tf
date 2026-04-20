e157e6a15653046ae25e52ebfb0aa24b2fdcb782
anjalik <kumarianjali@gmail.com>
2026-04-20 11:48:34 +0530

    updated port in main.tf

--- main.tf
+++ main.tf
@@ -47,8 +47,8 @@ resource "aws_security_group" "ecs_sg" {
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
